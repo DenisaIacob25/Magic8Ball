b0e2fdb06f32e5c8000dc6087ccf227e6570d7f2
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-22 14:55:19 -0500

    MVP commit

--- Magic8BallProject/8Ball.js
+++ Magic8BallProject/8Ball.js
@@ -1,1 +1,18 @@
 ﻿
+
+var outcome = [
+    "get bitten by an Crocodile while visiting the Amazon.",
+    "step on a jellyfish while visiting the great barrier reef.",
+    "fall off the great wall of China.",
+    "get sick drinking water in Mexico.",
+    "get stranded in an avalanche in Switzerland.",
+    "witness the Northern Lights.",
+    "visit Machu Pichu",
+    "drink beer in Germany during Octoberfest.",
+    "win 10 million dollars gambling in Monaco."]
+
+function UpdateDiv() {
+    document.getElementById("result").innerHTML = outcome[Math.floor(Math.random() * outcome.length)];
+
+}
+
